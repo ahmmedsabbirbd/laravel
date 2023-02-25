97acc1f9f14632e99a70f3ef90ddaba7d1a919c7
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-02-25 20:41:32 +0600

    products

---
+++ module-5/products/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('products.php');
+require_once('members.php');
+
+$prouctsList = $products = [
+    ['id' => 12143, 'name' => 'Product 1', 'price' => 10.99],
+    ['id' => 12141, 'name' => 'Product 2', 'price' => 5.99],
+    ['id' => 12151, 'name' => 'Product 3', 'price' => 7.99],
+];
+
+$products = new Products($prouctsList);
+
+// print_r($products->getProducts());
+
+
+$shop = new Shop($products->getProducts());
+$shop->addShoped(12141);
+
+print_r($shop->shopedProduct());
---
+++ module-5/products/members.php
@@ -0,0 +1,17 @@
+<?php
+
+class Members {
+    private $members;
+
+    function __construct() {
+        $this->members = [];
+    }
+
+    function setName($name) {
+        $this->members[] = $name;
+    }
+    
+    function getName() {
+        return $this->members;
+    }
+}
