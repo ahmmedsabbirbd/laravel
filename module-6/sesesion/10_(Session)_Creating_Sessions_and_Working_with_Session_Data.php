140807b07d2041d8bd6b8f9938a19451676a3c68
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-03-11 15:13:21 +0600

    session good understand

--- module-6/sesesion/10_(Session)_Creating_Sessions_and_Working_with_Session_Data.php
+++ module-6/sesesion/10_(Session)_Creating_Sessions_and_Working_with_Session_Data.php
@@ -1,1 +1,13 @@
 <?php
+session_name("session_app");
+session_id("sessionId");
+session_start([
+    "cookie_lifetime"=>10,
+    // "cookie_domain" => '.sabbir.com',
+    // "cookie_path" => '/'
+]);
+// $_SESSION['name'] = "Sabbir";
+
+if( $_SESSION['name'] ) {   
+    echo $_SESSION['name'];
+}
